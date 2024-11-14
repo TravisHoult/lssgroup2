e914ca92f003130d21b3f7684670a98b4649d1b1
TravisHoult <20221016@students.latrobe.edu.au>
2024-11-14 15:47:42 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello World</h1>
+            </header>
+        </div>
+    );
+}
+export default App;
