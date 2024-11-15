e9a1ceb562c2b1500d3a92c42c19203a741f5f92
Joshua H <20961581@students.latrobe.edu.au>
2024-11-15 20:32:29 +1100

    Added File Admin, Manager and Lecturer
    Created and added the files from the admin, manager and lecturer

---
+++ src/Components/admin.jsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+const Admin = () => {
+    return <h1>Admin Dashboard</h1>;
+};
+
+export default Admin;
---
+++ src/Components/lecture.jsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+const Lecturer = () => {
+    return <h1>Admin Dashboard</h1>;
+};
+
+export default Lecturer;
